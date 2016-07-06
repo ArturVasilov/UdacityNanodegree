7b13a2688563789a49e0275c29aac8086c350b8f
AVasilov <artur.vasilov@dz.ru>
2016-07-06 18:15:25 +0300

    added stockhawk sample

---
+++ StockHawk/app/src/main/java/com/sam_chordas/android/stockhawk/touch_helper/ItemTouchHelperAdapter.java
@@ -0,0 +1,11 @@
+package com.sam_chordas.android.stockhawk.touch_helper;
+
+/**
+ * Created by sam_chordas on 10/6/15.
+ * credit to Paul Burke (ipaulpro)
+ * Interface to enable swipe to delete
+ */
+public interface ItemTouchHelperAdapter {
+
+    void onItemDismiss(int position);
+}
