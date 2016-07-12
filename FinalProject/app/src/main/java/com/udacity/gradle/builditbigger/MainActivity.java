9a1f49143e481fbbeeb8dee30280bd523d322f8c
AVasilov <artur.vasilov@dz.ru>
2016-07-12 10:45:51 +0300

    initial commit for Build it Bigger project

---
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -0,0 +1,17 @@
+package com.udacity.gradle.builditbigger;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * @author Artur Vasilov
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ FinalProject/app/src/main/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -0,0 +1,43 @@
+package com.udacity.gradle.builditbigger;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+/**
+ * @author Artur Vasilov
+ */
+public class MainActivityFragment extends Fragment implements View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+
+        AdView mAdView = (AdView) root.findViewById(R.id.adView);
+        // Create an ad request. Check logcat output for the hashed device ID to
+        // get test ads on a physical device. e.g.
+        // "Use AdRequest.Builder.addTestDevice("ABCDEF012345") to get test ads on this device."
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+        root.findViewById(R.id.tellJokeButton).setOnClickListener(this);
+
+        return root;
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.tellJokeButton) {
+            Toast.makeText(getActivity(), "TODO", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
