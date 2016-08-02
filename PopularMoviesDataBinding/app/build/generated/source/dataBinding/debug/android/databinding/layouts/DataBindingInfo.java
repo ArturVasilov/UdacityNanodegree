96c9d88750a7a0751a2706c21dcef9b0f8309fdb
Artur Vasilov <artur.vasilov@dz.ru>
2016-08-02 13:57:06 +0300

    clean up repository

--- PopularMoviesDataBinding/app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++
@@ -1,6 +0,0 @@
-package android.databinding.layouts;
-
-import android.databinding.BindingBuildInfo;
-
-@BindingBuildInfo(buildId="428ff0cd-3c8f-4bba-8222-d270aa1acc72", modulePackage="arturvasilov.udacity.nanodegree.popularmoviesdatabinding", sdkRoot="D:\\Android\\sdk",layoutInfoDir="D:\\Git\\Github\\UdacityNanodegree\\PopularMoviesDataBinding\\app\\build\\intermediates\\data-binding-info\\debug",exportClassListTo="",isLibrary=false,minSdk=16,enableDebugLogs=false,printEncodedError=true)
-public class DataBindingInfo {}
