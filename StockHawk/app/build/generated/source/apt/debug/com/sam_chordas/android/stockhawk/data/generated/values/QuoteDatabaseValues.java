ae1d64eb9c3ecdcc9506f0cdb1be54a996365f99
Artur Vasilov <artur.vasilov@dz.ru>
2016-08-07 19:22:42 +0300

    fixed gitignore

--- StockHawk/app/build/generated/source/apt/debug/com/sam_chordas/android/stockhawk/data/generated/values/QuoteDatabaseValues.java
+++
@@ -1,7 +0,0 @@
-package com.sam_chordas.android.stockhawk.data.generated.values;
-
-public class QuoteDatabaseValues {
-  public QuotesValuesBuilder forQuotes() {
-    return new QuotesValuesBuilder();
-  }
-}
